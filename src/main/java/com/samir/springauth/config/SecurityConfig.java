033348df4fddbf2f7ad56688fb559295dd238e7b
Samir1498 <samirsbx2@gmail.com>
2023-12-25 11:40:27 +0100

    removing cors bean

--- src/main/java/com/samir/springauth/config/SecurityConfig.java
+++ src/main/java/com/samir/springauth/config/SecurityConfig.java
@@ -11,9 +11,6 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 
 @Configuration
 @EnableWebSecurity
@@ -45,7 +42,7 @@ public class SecurityConfig {
                 .addFilterBefore(
                         jwtTokenFilter,
                         UsernamePasswordAuthenticationFilter.class
-                );;
+                );
 
         return http.build();
     }
