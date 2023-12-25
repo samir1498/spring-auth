fd7d9688d5df58f5e038d34c54a4021150569405
Samir1498 <samirsbx2@gmail.com>
2023-12-25 08:55:38 +0100

    creating spring security config class

---
+++ src/main/java/com/samir/springauth/config/SecurityConfig.java
@@ -0,0 +1,45 @@
+package com.samir.springauth.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.context.HttpSessionSecurityContextRepository;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+@EnableMethodSecurity
+public class SecurityConfig {
+
+    private static final String[] WHITE_LIST_URL = {"/api/v1/auth/**"};
+    private final AuthenticationProvider authenticationProvider;
+    private final HttpSessionSecurityContextRepository httpSessionSecurityContextRepository;
+
+
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(req ->
+                        req.requestMatchers(WHITE_LIST_URL)
+                                .permitAll()
+                                .requestMatchers("api/v1/users").hasRole("USER")
+                                .requestMatchers("api/v1/admins").hasRole("ADMIN")
+                                .anyRequest()
+                                .authenticated()
+                )
+                .securityContext(context -> context.securityContextRepository(httpSessionSecurityContextRepository))
+                .authenticationProvider(authenticationProvider);
+
+        return http.build();
+    }
+
+}
